6386aaf77af6fdd01299a7304ae102d83e28f5a2
David Clark <dclark@pvsouth.local>
2017-09-06 10:25:18 -0700

    code cleanup

--- PS_get_gcp_checkpoint_errors.py
+++ PS_get_gcp_checkpoint_errors.py
@@ -1,5 +1,8 @@
 # Script for finding the error at each GCP in a PhotoScan model.
-# This script enables all the markers, then turns off one at a time
+# This script only runs inside Agisoft PhotoScan Pro.
+# This script iterates through all enabled markers (with valid coordinates), 
+# turning off one at a time, optimizing the cameras, and checking the errors.
+# The errors are written to a text file in the same directory as the .psx file.
 
 import PhotoScan
 
@@ -15,75 +18,31 @@ print('\nrunning:\n    PS_get_gcp_checkpoint_errors.py\n')
 
 
 def getOptimizeParams(chunk):
-    # TODO: trying getting params here instead:
-    #            PhotoScan.app.document.chunk.meta['optimize/fit_flags']
-    
-    # this section checks the camera calibration for non-zero parameters.
-    # There are some problems with this because PhotoScan doesn't zero out parameters
-    # when you unselect them in optimizeCameras, so this will choose all previous
-    # parameters you've used
-    '''
-    paramList = ['f',
-                 'cx',
-                 'cy',
-                 'b1',
-                 'b2',
-                 'k1',
-                 'k2',
-                 'k3',
-                 'k4',
-                 'p1',
-                 'p2',
-                 'p3',
-                 'p4']  # this is missing 'fit_shutter' 
-    paramDict = {}
-    for ii, param in enumerate(paramList):
-        tf = not(getattr(chunk.sensors[0].calibration,param)==0)
-        paramDict['fit_{}'.format(param)] = tf
-    paramDict['fit_shutter'] = False
-    return paramDict
-    '''
     # get enabled optimizeCamera parameters from chunk.meta
-    paramList = ['f',
-                 'cx',
-                 'cy',
-                 'b1',
-                 'b2',
-                 'k1',
-                 'k2',
-                 'k3',
-                 'k4',
-                 'p1',
-                 'p2',
-                 'p3',
-                 'p4',
-                 'shutter']
+    paramList = [
+        'f', 'cx', 'cy', 'b1', 'b2', 'k1', 'k2', 'k3', 'k4', 'p1', 'p2', 'p3',
+        'p4', 'shutter'
+    ]
+
     enabledList = chunk.meta['optimize/fit_flags']
     enabledList2 = enabledList.split()
+
     paramDict = {}
     for param in paramList:
         if param in enabledList2:
             paramDict['fit_{}'.format(param)] = True
         else:
             paramDict['fit_{}'.format(param)] = False
-#     print('enabledList = '+enabledList)
-#     print('paramDict = ')
-    for a, b in paramDict.items():
-        print(a,b)
-    return paramDict        
-    
-    
+    return paramDict
+
 
 for chunk in PhotoScan.app.document.chunks:
 
     print('Processing chunk: {}'.format(chunk.label))
-#     print('Enabling all markers')
     outfile.write('chunk = {}\n'.format(chunk.label))
 
-#     marker_list = []
-#     # check which markers are enabled
-#     for marker in chunk.markers:
-#         marker_list.append(marker.reference.enabled)
+    # get the last optimizeCameras parameters used
+    optimizeParamsDict = getOptimizeParams(chunk)
 
     errorList = []
     errorList2 = []
@@ -91,33 +50,12 @@ for chunk in PhotoScan.app.document.chunks:
     for marker in chunk.markers:
         if marker.reference.enabled and marker.projections.values():
             print('processing marker:', marker.label)
-           #  print(['marker.position',marker.position])
-#             print(['marker.projections.items()',marker.projections.items()])
-#             print(['marker.projections.values()',marker.projections.values()])
             markerList.append(marker.label)
             # turn off the marker to find the error
             marker.reference.enabled = False
-            # print(['marker.position',marker.position])
-#             print(['marker.projections.items()',marker.projections.items()])
-#             print(['marker.projections.values()',marker.projections.values()])
             # re-optimize the cameras without the marker
-            # TODO: figure out how to use the last users selected parameters in 
-            #       Optimize cameras
-            # This guesses the current optimize params from the adjusted calibration,
-            # but doens't include the rolling shutter (set to False)
-            optimizeParamsDict = getOptimizeParams(chunk)
             chunk.optimizeCameras(**optimizeParamsDict)
-            #     fit_f=True,
-#                 fit_cxcy=True,
-#                 fit_b1=True,
-#                 fit_b2=True,
-#                 fit_k1k2k3=True,
-#                 fit_p1p2=True,
-#                 fit_k4=True,
-#                 fit_p3=False,
-#                 fit_p4=False)
-            # get the error
-            # measured values in geocentric coordinates
+            # measured marker locations in geocentric coordinates
             source = chunk.crs.unproject(marker.reference.location)
             # estimated coordinates in geocentric coordinates
             estim = chunk.transform.matrix.mulp(marker.position)
@@ -129,11 +67,12 @@ for chunk in PhotoScan.app.document.chunks:
             errorList2.append(error.norm()**2)  # list of squared errors
             # turn the marker back on
             marker.reference.enabled = True
-            outfile.write(
-                '   marker {}:  error = {}\n'.format(marker.label, error.norm()))
+            outfile.write('   marker {}:  error = {}\n'.format(
+                marker.label, error.norm()))
             outfile.flush()
         elif marker.reference.enabled and not marker.projections.values():
-            print('Marker {} not processed, no projections'.format(marker.label))
+            print(
+                'Marker {} not processed, no projections'.format(marker.label))
 
     suma = sum(errorList2)
     n = len(errorList)
@@ -144,29 +83,15 @@ for chunk in PhotoScan.app.document.chunks:
     outfile.flush()
 
     # re-optimize the cameras with all markers enabled
-    chunk.optimizeCameras(**optimizeParamsDict)  
-#         fit_f=True,
-#         fit_cxcy=True,
-#         fit_b1=True,
-#         fit_b2=True,
-#         fit_k1k2k3=True,
-#         fit_p1p2=True,
-#         fit_k4=True,
-#         fit_p3=False,
-#         fit_p4=False)
-    print('\n')
-    
+    chunk.optimizeCameras(**optimizeParamsDict)
+
     # write the errors to the console
+    print('\n')
     print('\nErrors for chunk {}:'.format(chunk.label))
     for ii, nm in enumerate(markerList):
         print('marker {}:  error = {}'.format(nm, errorList[ii]))
-    print('\n total rms error in chunk {} = {}\n'.format(chunk.label, rmsError))
+    print(
+        '\n total rms error in chunk {} = {}\n'.format(chunk.label, rmsError))
 
 outfile.close()
-
-
-
-
 print('Errors written to file: {}{}'.format(fName, '_ERRORS.txt'))
-
-# str(sys.argv)
