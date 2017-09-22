3ac98abaf092bca0c24ff36bbb5f95e4fac4b20a
David Clark <dclark@pvsouth.local>
2017-09-22 15:08:36 -0700

    finished the arg parsing, changed default no-arg direcotry to os.getcwd() so hopefully it'll work on windoze too (not tested)

--- DJI_drone_barometer_altitide_for_PhotoScan.py
+++ DJI_drone_barometer_altitide_for_PhotoScan.py
@@ -10,8 +10,11 @@
 
 import glob
 import sys
-from PIL import Image
+import os
 import argparse 
+
+from PIL import Image
+
 
 
 def get_exif_gps(image):
@@ -23,7 +26,7 @@ def get_exif_gps(image):
             gps_out['lat'] = (float(exif[34853][2][0][0])/float(exif[34853][2][0][1])
                         + float(exif[34853][2][1][0])/float(exif[34853][2][1][1])/60
                         + float(exif[34853][2][2][0])/float(exif[34853][2][2][1])/3600)
-            if exif[34853][1][0][0] == 'N':
+            if exif[34853][1][0][0] == 'S':
                 gps_out['lat'] = -gps_out['lat']
             # longitude in decimal degrees
             gps_out['lon'] = (float(exif[34853][4][0][0])/float(exif[34853][4][0][1])
@@ -57,6 +60,8 @@ def get_xmp_dji(image):
                 # gimble pitch degrees
                 dji['pitch'] = (
                     float(c.split('drone-dji:GimbalPitchDegree=\"')[1].split('\"')[0]))
+                # add 90 to convert from DJI reference frame to PhotoScan frame    
+                dji['pitch'] = dji['pitch']+90
                 # gimble roll degrees
                 dji['roll'] = (
                     float(c.split('drone-dji:GimbalRollDegree=\"')[1].split('\"')[0]))
@@ -70,122 +75,56 @@ parser = argparse.ArgumentParser(description='This script pulls GPS data, DJI gi
                                    +'stores them in a text file for easy input into '
                                    +'Agisoft PhotoScan',
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-r', '--recursive', action='store_true', dafault=False, 
+parser.add_argument('-r', '--recursive', action='store_true', default=False, 
                     help='search for DJI jpegs recursively')
 parser.add_argument('-i', '--input',action='store',type=str,default='./',
-                    help=('input directory to search for DJI jepgs (recursively with -r), '
+                    help=('input directory to search for DJI jepgs (recursive with -r), '
                             +'or an input filename'))
 parser.add_argument('-o','--output', action='store',type=str,
                     default='[output_directory]/DJI_camera_poses.txt',
-                    help='file path/name for output text file')              
-
-fname  = sys.argv[1]
-with Image.open(fname) as im:
-    dji = get_xmp_dji(im)
-    gps = get_exif_gps(im)
-    print(dji)
-    print(gps)
+                    help='file path/name for output text file') 
+                    
+input_args = parser.parse_args()                                 
 
 
+# get the file names
+path = input_args.input
+# I think this should work on a Windoze machine ... not sure, I hatez windoze  
+if path=='./' or path=='.':
+    path = os.getcwd()
+if path[-1]=='/':
+    path = path[:-1]
+if os.path.isdir(path):
+    isdir = True
+    if input_args.recursive:
+        fnames = glob.iglob(path+'/**/DJI*.JPG', recursive=True)
+    else:
+        fnames = glob.iglob(path+'/DJI*.JPG', recursive=False)    
+elif os.path.isfile(path):
+    fnames = glob.iglob(path)
+    isdir = False
+else:
+    print('could not find file or directory')
+    sys.exit()
 
-# 
-# basedir = sys.argv[1]
-# if basedir[-1]=='/':
-#     basedir = basedir[:-1]
-# 
-# for fname in glob.iglob(basedir+'/**/DJI_*.JPG', recursive=True):
-#     
-# # 34853    1   GPSInfo Exif.GPSInfo.GPSLatitudeRef, 'N' or 'S'
-# # 34853    2   GPSInfo Exif.GPSInfo.GPSLatitude
-# # 34853    3   GPSInfo Exif.GPSInfo.GPSLongitudeRef, 'E' or 'W'
-# # 34853    4   GPSInfo Exif.GPSInfo.GPSLongitude
-# # 34853    5   GPSInfo Exif.GPSInfo.GPSAltitudeRef,  '0' above sea level or '1' below
-# # 34853    6   GPSInfo Exif.GPSInfo.GPSAltitude
-#     
-# #GPS info 34853
-# #Lat deg,  float(info[34853][2][0][0])/float(info[34853][2][0][1]) 
-# #Lat min,  float(info[34853][2][1][0])/float(info[34853][2][1][1]) 
-# #Lat sec,  float(info[34853][2][2][0])/float(info[34853][2][2][1])    
-#     
-# # check out this example of exif using PIL 
-# # https://gist.github.com/erans/983821
-#     
-# # get the XMP data
-# '''
-# with Image.open(filename) as im:
-#     for segment, content in im.applist:
-#         marker, body = content.split(b'\x00', 1)
-#         if segment == 'APP1' and marker == b'http://ns.adobe.com/xap/1.0/':
-#             c = str(content)
-#             print( (c.split('drone-dji:GimbalPitchDegree=\"'))[1].split('\"')[0])
-#             
-# drone-dji:RelativeAltitude
-# drone-dji:GimbalRollDegree
-# drone-dji:GimbalYawDegree
-# drone-dji:GimbalPitchDegree            
-# '''            
-# 
-# def get_exif_data(image):
-#     """Returns a dictionary from the exif data of an PIL Image item. Also converts the GPS Tags"""
-#     exif_data = {}
-#     info = image._getexif()
-#     if info:
-#         for tag, value in info.items():
-#             decoded = TAGS.get(tag, tag)
-#             if decoded == "GPSInfo":
-#                 gps_data = {}
-#                 for t in value:
-#                     sub_decoded = GPSTAGS.get(t, t)
-#                     gps_data[sub_decoded] = value[t]
-# 
-#                 exif_data[decoded] = gps_data
-#             else:
-#                 exif_data[decoded] = value
-# 
-#     return exif_data
-# 
-# def _get_if_exist(data, key):
-#     if key in data:
-#         return data[key]
-# 		
-#     return None
-# 	
-# def _convert_to_degress(value):
-#     """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
-#     d0 = value[0][0]
-#     d1 = value[0][1]
-#     d = float(d0) / float(d1)
-# 
-#     m0 = value[1][0]
-#     m1 = value[1][1]
-#     m = float(m0) / float(m1)
-# 
-#     s0 = value[2][0]
-#     s1 = value[2][1]
-#     s = float(s0) / float(s1)
-# 
-#     return d + (m / 60.0) + (s / 3600.0)
-# 
-# def get_lat_lon(exif_data):
-#     """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
-#     lat = None
-#     lon = None
-# 
-#     if "GPSInfo" in exif_data:		
-#         gps_info = exif_data["GPSInfo"]
-# 
-#         gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
-#         gps_latitude_ref = _get_if_exist(gps_info, 'GPSLatitudeRef')
-#         gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
-#         gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
-# 
-#         if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
-#             lat = _convert_to_degress(gps_latitude)
-#             if gps_latitude_ref != "N":                     
-#                 lat = 0 - lat
-# 
-#             lon = _convert_to_degress(gps_longitude)
-#             if gps_longitude_ref != "E":
-#                 lon = 0 - lon
-# 
-#     return lat, lon
+# output text file name
+if input_args.output == '[output_directory]/DJI_camera_poses.txt':
+    if isdir:
+        fname_out = (path + '/DJI_camera_poses.txt')
+    else:
+        fname_out = (path.rsplit('/',1)[0] + '/DJI_camera_poses.txt')
+else:
+    fname_out = input_args.output
+
+
+with open(fname_out,'w') as fout:
+    fout.write('File, Lat, Lon, GPS_altitude, Relative_altitude, yaw, pitch, roll\n')
+    for file in fnames:
+        print(file)    
+        with Image.open(file) as im:
+            dji = get_xmp_dji(im)
+            gps = get_exif_gps(im)
+            fout.write('{}, {:.7f}, {:.7f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}\n'.format(
+                 os.path.abspath(file), gps['lat'], gps['lon'], gps['alt'], 
+                 dji['rel_alt'], dji['yaw'], dji['pitch'], dji['roll']))
+
